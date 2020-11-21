42e800b7094dc59aa666d0646dbb54f1ff77f04c
unknown <danyu999@gmail.com>
2020-11-21 16:11:25 -0500

    Finish implementing cuckoo concurrent

--- src/cuckoo-concurrent.h
+++ src/cuckoo-concurrent.h
@@ -4,6 +4,7 @@
 #include <functional>
 #include <ctime>
 #include <list>
+#include <mutex>
 
 template <class T>
 class CuckooConcurrentHashSet {
@@ -14,6 +15,7 @@ class CuckooConcurrentHashSet {
     size_t salt1;
     int capacity;
     std::vector<std::vector<std::list<T>>> table;
+    std::vector<std::vector<std::recursive_mutex>> locks;
 
     template <class T>
     std::list<int>::iterator find(const std::list<T> &l, const T &value) {
@@ -31,59 +33,126 @@ class CuckooConcurrentHashSet {
         size_t seed = 0;
         hash_combine(seed, val);
         hash_combine(seed, salt0);
-        return seed % capacity;
+        return seed;
     }
 
     int hash1(T val) {
         size_t seed = 0;
         hash_combine(seed, val);
         hash_combine(seed, salt1);
-        return seed % capacity;
+        return seed;
     }
 
     bool relocate(int i, int hi) {
-            
+        int hj = 0;
+        int j = 1 - i;
+        for (int round = 0; round < limit; round++) {
+            std::list<int> i_set = table[i][hi];
+            T val = i_set.front();
+            switch (i) {
+                case 0: hj = hash1(val) % capacity; break;
+                case 1: hj = hash0(val) % capacity; break;
+            }
+            acquire(val);
+            std::list<int> j_set = table[j][hj];
+            if ((auto it = find(i_set, val) != i_set.end()) {
+                i_set.erase(it);
+                if (j_set.size() < THRESHOLD) {
+                    j_set.push_back(val);
+                    release(val);
+                    return true;
+                } else if (j_set.size() < PROBE_SIZE) {
+                    j_set.push_back(val);
+                    i = 1 - i;
+                    hi = hj;
+                    j = 1 - j;
+                    release(val);
+                } else {
+                    i_set.push_back(val);
+                    release(val);
+                    return false;
+                }
+            } else if (i_set.size() >= THRESHOLD) {
+                release(val);
+                continue;
+            } else {
+                release(val);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    void acquire(T val) {
+        locks[0][hash0(val) % locks[0].size()].lock();
+        locks[1][hash1(val) % locks[1].size()].lock();
+    }
+
+    void release(T val) {
+        locks[0][hash0(val) % locks[0].size()].unlock();
+        locks[1][hash1(val) % locks[1].size()].unlock();
     }
 
     /**
      * Resizes the table to be twice as big. Changes salt0 and salt1.
      */
-    void resize() { //TODO make concurrent
-        std::cout << "resize" << std::endl;
+    void resize() {
+        //std::cout << "resize" << std::endl;
+        int oldCapacity = capacity;
+        // Since we have consistent ordering when acquiring locks, we only need
+        // to acquire the locks for table0.
+        for (auto lock : locks[0]) {
+            lock.lock();
+        }
+
+        // Another resize happened
+        if (capacity != oldCapacity)
+            return;
+        
         // Get new salt values to change the hashes
         hash_combine(salt0, time(NULL));
         hash_combine(salt1, time(NULL));
 
         capacity *= 2;
         limit *= 2;
-        std::vector<std::vector<Entry*>> old_table(table);
+        std::vector<std::vector<std::list<T>>> old_table(table);
         table.clear();
         for (int i = 0; i < 2; i++) {
-            std::vector<Entry*> row;
-            row.assign(capacity, nullptr);
+            std::vector<std::list<T>> row;
+            for (int j = 0; j < capacity; j++) {
+                std::list<T> probe_set;
+                row.push_back(probe_set);
+            }
             table.push_back(row);
         }
 
         // Add the elements back into the bigger table
         for (auto row : old_table) {
-            for (auto entry : row) {
-                if (entry != nullptr) {
+            for (auto probe_set : row) {
+                for (auto entry : probe_set) {
                     add(entry->val); //TODO: what if this add call calls resize again? segfault
                     delete entry;
                 }
             }
         }
+
+        // Release locks
+        for (auto lock : locks[0])
+            lock.unlock();
     }
 
     public:
         CuckooConcurrentHashSet(int capacity) : capacity(capacity), limit(capacity/2) {
             for (int i = 0; i < 2; i++) {
                 std::vector<std::list<T>> row;
+                std::vector<std::recursive_mutex> locks_row;
                 for (int j = 0; j < capacity; j++) {
                     std::list<T> probe_set;
                     row.push_back(probe_set);
+                    locks_row.emplace_back(new std::recursive_mutex());
                 }
                 table.push_back(row);
+                locks.push_back(locks_row);
             }
             salt0 = time(NULL);
             salt1 = salt0;
@@ -91,16 +160,15 @@ class CuckooConcurrentHashSet {
         }
 
         ~CuckooConcurrentHashSet() {
-            for (auto row : table) {
-                for (auto probe_set : row) {
-                    for (auto entry : probe_set) {
-                        if (entry != nullptr) {
-                            delete entry;
-                        }
+            for (int i = 0; i < 2; i++) {
+                for (int j = 0; j < capacity; j++) {
+                    for (auto entry : table[i][j]) {
+                        delete entry;
                     }
-                    probe_set.clear();
+                    delete locks[i][j];
+                    table[i][j].clear();
                 }
-                row.clear();
+                table[i].clear();
             }
             table.clear();
         }
@@ -111,7 +179,7 @@ class CuckooConcurrentHashSet {
          */
         bool add(T val) {
             acquire(val);
-            int h0 = hash0(val), h1 = hash1(val);
+            int h0 = hash0(val) % capacity, h1 = hash1(val) % capacity;
             int i = -1, h = -1;
             bool mustResize = false;
             if (contains(val)) {
@@ -156,13 +224,13 @@ class CuckooConcurrentHashSet {
          */
         bool remove(T val) {
             acquire(val);
-            std::list<T> set0 = table[0][hash0(val)];
+            std::list<T> set0 = table[0][hash0(val) % capacity];
             if ((auto it = find(set0, val)) != set0.end()) {
                 set0.erase(it);
                 release(val);
                 return true;
             } else {
-                std::list<T> set1 = table[1][hash1(val)];
+                std::list<T> set1 = table[1][hash1(val) % capacity];
                 if ((auto it = find(set1, val)) != set1.end()) {
                     set1.erase(it);
                     release(val);
@@ -180,12 +248,12 @@ class CuckooConcurrentHashSet {
         bool contains(T val) {
             //std::cout << "contains" << std::endl;
             acquire(val);
-            std::list<T> set0 = table[0][hash0(val)];
+            std::list<T> set0 = table[0][hash0(val) % capacity];
             if ((auto it = find(set0, val)) != set0.end()) {
                 release(val);
                 return true;
             } else {
-                std::list<T> set1 = table[1][hash1(val)];
+                std::list<T> set1 = table[1][hash1(val) % capacity];
                 if ((auto it = find(set1, val)) != set1.end()) {
                     release(val);
                     return true;
@@ -199,11 +267,11 @@ class CuckooConcurrentHashSet {
          * Counts the number of elements in the table
          * return: The number of elements in the table
          */
-        int size() { //TODO: make fit new data structure
+        int size() {
             int size = 0;
-            for (int i = 0; i < 2; i++) {
-                for (int j = 0; j < capacity; j++) {
-                    if (table[i][j] != nullptr) {
+            for (auto row : table) {
+                for (auto probe_set : row) {
+                    for (auto entry : probe_set) {
                         size++;
                     }
                 }
