69d626b438f885ba19fd9e78525a7031e39613d6
Daniel Yu <danyu999@gmail.com>
2020-11-21 16:59:04 -0500

    Finish cuckoo-test for concurrent cuckoo

--- src/cuckoo-test.cc
+++ src/cuckoo-test.cc
@@ -7,12 +7,13 @@
 #include <assert.h>
 
 #include "cuckoo-serial.h"
-//#include "cuckoo-concurrent.h"
-//#include "cuckoo-transactional.h"
+#include "cuckoo-concurrent.h"
+#include "cuckoo-transactional.h"
 
 const int NUM_OPS = 1000000;
 const int CAPACITY = 1000;
 const int KEY_MAX = 10000;
+const int NUM_THREADS = 1;
 
 struct Operation {
     int val;
@@ -21,7 +22,7 @@ struct Operation {
 };
 
 struct Metrics {
-    long long exec_time;
+    long long exec_time = 0;
     int contains_hit = 0;
     int contains_miss = 0;
     int add_hit = 0;
@@ -47,7 +48,7 @@ std::vector<int> generate_entries(int num_entries) {
     return {entries.begin(), entries.end()};
 }
 
-std::vector<Operation> generate_operations(int num_ops, std::vector<int> entries) {
+std::vector<Operation> generate_operations(int num_ops, std::vector<int> *entries) {
     // 50% contains, 25% add, 25% remove
     // With guarenteed success for add and remove operations, size of table should stay
     // relatively the same.
@@ -59,7 +60,7 @@ std::vector<Operation> generate_operations(int num_ops, std::vector<int> entries
     std::uniform_int_distribution<int> distribution_entries(0, KEY_MAX);
     std::vector<Operation> ops;
     for (int i = 0; i < num_ops; i++) {
-        std::uniform_int_distribution<int> distribution_existing_entries(0, entries.size()-1);
+        std::uniform_int_distribution<int> distribution_existing_entries(0, entries->size()-1);
         int which_op = distribution_percentage(generator);
         if (which_op <= 50) {
             // contains
@@ -67,7 +68,7 @@ std::vector<Operation> generate_operations(int num_ops, std::vector<int> entries
         } else if (which_op <= 75) {
             // add
             int entry = distribution_entries(generator);
-            entries.push_back(entry);
+            entries->push_back(entry);
             ops.emplace_back(entry, 1);
         } else {
             // remove
@@ -116,8 +117,42 @@ void do_work_serial(CuckooSerialHashSet<int> *cuckoo_serial, std::vector<Operati
 /**
  * Runs a workload for cuckoo concurrent
  */
-void do_work_concurrent() {
+void do_work_concurrent(CuckooConcurrentHashSet<int> *cuckoo_concurrent, std::vector<Operation> ops, std::vector<Metrics> *concurrent_metrics) {
+    Metrics metrics = {};
+    // Start doing work
+    long long exec_time_start = std::chrono::high_resolution_clock::now().time_since_epoch().count();
+    for (auto op : ops) {
+        // std::cout << "val: " << op.val << ", type: " << op.type << std::endl;
+        switch (op.type) {
+            // Contains
+            case 0:
+                if (cuckoo_concurrent->contains(op.val))
+                    metrics.contains_hit++;
+                else
+                    metrics.contains_miss++;
+                break;
+            // Insert
+            case 1:
+                if (cuckoo_concurrent->add(op.val))
+                    metrics.add_hit++;
+                else
+                    metrics.add_miss++;
+                break;
+            // Remove
+            default:
+                if (cuckoo_concurrent->remove(op.val))
+                    metrics.remove_hit++;
+                else
+                    metrics.remove_miss++;
+                break;
+        }
+    }
+    long long exec_time_end = std::chrono::high_resolution_clock::now().time_since_epoch().count();
+	metrics.exec_time = exec_time_end - exec_time_start;
 
+    std::mutex lock();
+    concurrent_metrics->push_back(metrics);
+    std::mutex unlock();
 }
 
 /**
@@ -128,17 +163,16 @@ void do_work_transactional() {
 }
 
 int main(int argc, char *argv[]) {
-    auto entries = generate_entries(CAPACITY/3);
-
     // Serial Cuckoo
+    std::cout << "Starting serial cuckoo..." << std::endl;
     CuckooSerialHashSet<int> *cuckoo_serial = new CuckooSerialHashSet<int>(CAPACITY);
     // Setup hash table and pre-generate workload
     Metrics serial_metrics = {};
-    if (!cuckoo_serial->populate(entries))
+    auto serial_entries = generate_entries(CAPACITY/3);
+    if (!cuckoo_serial->populate(serial_entries))
         return 0;
-    auto ops = generate_operations(NUM_OPS, entries);
-    std::cout << "Starting serial cuckoo..." << std::endl;
-    do_work_serial(cuckoo_serial, ops, serial_metrics);
+    auto serial_ops = generate_operations(NUM_OPS, &serial_entries);
+    do_work_serial(cuckoo_serial, serial_ops, serial_metrics);
     int expected_size = CAPACITY/3 + serial_metrics.add_hit - serial_metrics.remove_hit;
     assert(expected_size == cuckoo_serial->size());
     std::cout << "Serial time (milliseconds):\t\t" << (double) serial_metrics.exec_time / 1000000.0 << std::endl;
@@ -148,22 +182,59 @@ int main(int argc, char *argv[]) {
     std::cout << "Serial add hit: " << serial_metrics.add_hit << std::endl;
     std::cout << "Serial add miss: " << serial_metrics.add_miss << std::endl;
     std::cout << "Serial remove hit: " << serial_metrics.remove_hit << std::endl;
-    std::cout << "Serial remove miss: " << serial_metrics.remove_miss << std::endl;
+    std::cout << "Serial remove miss: " << serial_metrics.remove_miss << std::endl << std::endl;
     delete cuckoo_serial;
 
     // Concurrent Cuckoo
-    //CuckooConcurrentHashSet<int> *cuckoo_concurrent = new CuckooConcurrentHashSet<int>(CAPACITY);
+    std::cout << "Starting concurrent cuckoo..." << std::endl;
+    CuckooConcurrentHashSet<int> *cuckoo_concurrent = new CuckooConcurrentHashSet<int>(CAPACITY);
+    auto concurrent_entries = generate_entries(CAPACITY/3);
+    if (!cuckoo_concurrent->populate(concurrent_entries))
+        return 0;
+    std::vector<std::vector<Operation>> concurrent_ops = std::vector<std::vector<Operation>>();
+    concurrent_ops.reserve(NUM_THREADS);
+    std::vector<std::thread> concurrent_threads = std::vector<std::thread>();
+	concurrent_threads.reserve(NUM_THREADS);
+    std::vector<Metrics> concurrent_metrics = std::vector<Metrics>();
+    concurrent_metrics.reserve(NUM_THREADS);
+    for (int thread = 0; thread < NUM_THREADS; thread++)
+        concurrent_ops.emplace_back(generate_operations(NUM_OPS, &concurrent_entries));
+    for (int thread = 0; thread < NUM_THREADS; thread++)
+        concurrent_threads.push_back(std::thread([&](){do_work_concurrent(cuckoo_concurrent, concurrent_ops[thread], &concurrent_metrics);}));
+    for (int thread = 0; thread < NUM_THREADS; thread++)
+        concurrent_threads[thread].join();
+    int expected_size = CAPACITY/3 + serial_metrics.add_hit - serial_metrics.remove_hit;
+    assert(expected_size == cuckoo_serial->size());
+    Metrics total_concurrent_metrics = {};
+    if (concurrent_metrics.size() != NUM_THREADS)
+        std::cerr << "Concurrent metrics is incorrect size: " << concurrent_metrics.size() << std::endl;
+    for (int thread = 0; thread < NUM_THREADS; thread++) {
+        double exec_time = (double) concurrent_metrics[thread].exec_time / (double) 1000000;
+        std::cout << "Time to execute (milliseconds):\t\t\t" << exec_time << std::endl;
+        total_concurrent_metrics.exec_time += (exec_time - total_concurrent_metrics.exec_time) / (thread + 1);
+        std::cout << "Concurrent contains hit: " << concurrent_metrics[thread].contains_hit << std::endl;
+        std::cout << "Concurrent contains miss: " << concurrent_metrics[thread].contains_miss << std::endl;
+        std::cout << "Concurrent add hit: " << concurrent_metrics[thread].add_hit << std::endl;
+        std::cout << "Concurrent add miss: " << concurrent_metrics[thread].add_miss << std::endl;
+        std::cout << "Concurrent remove hit: " << concurrent_metrics[thread].remove_hit << std::endl;
+        std::cout << "Concurrent remove miss: " << concurrent_metrics[thread].remove_miss << std::endl << std::endl;
+        total_concurrent_metrics.contains_hit += concurrent_metrics[thread].contains_hit;
+        total_concurrent_metrics.contains_miss += concurrent_metrics[thread].contains_miss;
+        total_concurrent_metrics.add_hit += concurrent_metrics[thread].add_hit;
+        total_concurrent_metrics.add_miss += concurrent_metrics[thread].add_miss;
+        total_concurrent_metrics.remove_hit += concurrent_metrics[thread].remove_hit;
+        total_concurrent_metrics.remove_miss += concurrent_metrics[thread].remove_miss;
+    }
+    std::cout << "Average concurrent exec_time (milliseconds):\t\t" << total_concurrent_metrics.exec_time << std::endl;
+    std::cout << std::fixed << "Average concurrent total throughput (ops/sec):\t\t" << (double) (NUM_OPS * NUM_THREADS) / (total_concurrent_metrics.exec_time / 1000.0) << std::endl;
+    std::cout << "Concurrent total contains hit: " << total_concurrent_metrics.contains_hit << std::endl;
+    std::cout << "Concurrent total contains miss: " << total_concurrent_metrics.contains_miss << std::endl;
+    std::cout << "Concurrent total add hit: " << total_concurrent_metrics.add_hit << std::endl;
+    std::cout << "Concurrent total add miss: " << total_concurrent_metrics.add_miss << std::endl;
+    std::cout << "Concurrent total remove hit: " << total_concurrent_metrics.remove_hit << std::endl;
+    std::cout << "Concurrent total remove miss: " << total_concurrent_metrics.remove_miss << std::endl;
+    delete cuckoo_concurrent;
 
     // Transactional Cuckoo
     //CuckooTransactionalHashSet<int> *cuckoo_transactional = new CuckooTransactionalHashSet<int>(CAPACITY);
-
-
-    // int capacity = 15;
-    // int val = 222;
-    // cuckoo_serial->add(val);
-    // std::cout << "test successful1: " << cuckoo_serial->contains(val) << std::endl;
-    // cuckoo_serial->resize();
-    // std::cout << "test successful2: " << cuckoo_serial->contains(val) << std::endl;
-    // cuckoo_serial->remove(val);
-    // std::cout << "test successful3: " << cuckoo_serial->contains(val) << std::endl;
 }
--- src/cuckoo-transactional.h
+++ src/cuckoo-transactional.h
@@ -10,9 +10,10 @@ class CuckooTransactionalHashSet {
     // Wrapper class for entries to allow for nullptr to be the default
     struct Entry {
         T val;
+        Entry(T val) : val(val) {}
     };
 
-    const int LIMIT;
+    int limit;
     size_t salt0;
     size_t salt1;
     int capacity;
@@ -39,8 +40,38 @@ class CuckooTransactionalHashSet {
         return seed % capacity;
     }
 
+    /**
+     * Resizes the table to be twice as big. Changes salt0 and salt1.
+     */
+    void resize() {
+        std::cout << "resize" << std::endl;
+        // Get new salt values to change the hashes
+        hash_combine(salt0, time(NULL));
+        hash_combine(salt1, time(NULL));
+
+        capacity *= 2;
+        limit *= 2;
+        std::vector<std::vector<Entry*>> old_table(table);
+        table.clear();
+        for (int i = 0; i < 2; i++) {
+            std::vector<Entry*> row;
+            row.assign(capacity, nullptr);
+            table.push_back(row);
+        }
+
+        // Add the elements back into the bigger table
+        for (auto row : old_table) {
+            for (auto entry : row) {
+                if (entry != nullptr) {
+                    add(entry->val); //TODO: what if this add call calls resize again? segfault
+                    delete entry;
+                }
+            }
+        }
+    }
+
     public:
-        CuckooTransactionalHashSet(int capacity) : capacity(capacity), LIMIT(capacity/2) {
+        CuckooTransactionalHashSet(int capacity) : capacity(capacity), limit(capacity/2) {
             for (int i = 0; i < 2; i++) {
                 std::vector<Entry*> row;
                 row.assign(capacity, nullptr);
@@ -52,12 +83,13 @@ class CuckooTransactionalHashSet {
         }
 
         ~CuckooTransactionalHashSet() {
-            for (int i = 0; i < 2; i++) {
-                for (int j = 0; j < capacity; j++) {
-                    if (table[i][j] != nullptr) {
-                        delete table[i][j];
+            for (auto row : table) {
+                for (auto entry : row) {
+                    if (entry != nullptr) {
+                        delete entry;
                     }
                 }
+                row.clear();
             }
             table.clear();
         }
@@ -77,14 +109,11 @@ class CuckooTransactionalHashSet {
          * return: true if add was successful
          */
         bool add(T val) {
-            //std::cout << "add" << std::endl;
             if (contains(val)) {
                 return false;
             }
-            //std::cout << "add post contains" << std::endl;
-            Entry *value = new Entry();
-            value->val = val;
-            for (int i = 0; i < LIMIT; i++) {
+            Entry *value = new Entry(val);
+            for (int i = 0; i < limit; i++) {
                 if ((value = swap(0, hash0(value->val), value)) == nullptr) {
                     return true;
                 } else if ((value = swap(1, hash1(value->val), value)) == nullptr) {
@@ -102,11 +131,11 @@ class CuckooTransactionalHashSet {
         bool remove(T val) {
             int index0 = hash0(val);
             int index1 = hash1(val);
-            if (table[0][index0]->val == val) {
+            if (table[0][index0] != nullptr && table[0][index0]->val == val) {
                 delete table[0][index0];
                 table[0][index0] = nullptr;
                 return true;
-            } else if (table[1][index1]->val == val) {
+            } else if (table[1][index1] != nullptr && table[1][index1]->val == val) {
                 delete table[1][index1];
                 table[1][index1] = nullptr;
                 return true;
@@ -120,40 +149,14 @@ class CuckooTransactionalHashSet {
          */
         bool contains(T val) {
             //std::cout << "contains" << std::endl;
-            if (table[0][hash0(val)] != nullptr && table[0][hash0(val)]->val == val) {
+            int index0 = hash0(val);
+            int index1 = hash1(val);
+            if (table[0][index0] != nullptr && table[0][index0]->val == val) {
                 return true;
-            } else if (table[1][hash1(val)] != nullptr && table[1][hash1(val)]->val == val) {
+            } else if (table[1][index1] != nullptr && table[1][index1]->val == val) {
                 return true;
             }
             return false;
-        }
-
-        /**
-         * Resizes the table to be twice as big. Changes salt0 and salt1.
-         */
-        void resize() {
-            // Get new salt values to change the hashes
-            hash_combine(salt0, time(NULL));
-            hash_combine(salt1, time(NULL));
-
-            capacity *= 2;
-            std::vector<std::vector<Entry*>> old_table(table);
-            table.clear();
-            for (int i = 0; i < 2; i++) {
-                std::vector<Entry*> row;
-                row.assign(capacity, nullptr);
-                table.push_back(row);
-            }
-
-            // Add the elements back into the bigger table
-            for (int i = 0; i < 2; i++) {
-                for (int j = 0; j < capacity/2; j++) {
-                    if (old_table[i][j] != nullptr) {
-                        add(old_table[i][j]->val);
-                        delete old_table[i][j];
-                    }
-                }
-            }
         }
 
         /**
@@ -162,9 +165,9 @@ class CuckooTransactionalHashSet {
          */
         int size() {
             int size = 0;
-            for (int i = 0; i < 2; i++) {
-                for (int j = 0; j < capacity; j++) {
-                    if (table[i][j] != nullptr) {
+            for (auto row : table) {
+                for (auto entry : row) {
+                    if (entry != nullptr) {
                         size++;
                     }
                 }
@@ -174,8 +177,15 @@ class CuckooTransactionalHashSet {
 
         /**
          * Populates the table to some predetermined size
+         * return: true if successful
          */
-        std::vector<T> populate(int num_entries) {
-
+        bool populate(std::vector<T> entries) {
+            for (T entry : entries) {
+                if (!add(entry)) {
+                    std::cout << "Duplicate entry attempted for populate!" << std::endl;
+                    return false;
+                }
+            }
+            return true;
         }
 };
